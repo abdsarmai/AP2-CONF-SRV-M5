27b71120e81d4e3ac382a428c497c6975a38399e
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-03-03 14:47:57 +0100

    Create install-brique.sh

---
+++ install-brique.sh
@@ -0,0 +1,4 @@
+#Installation de python3 et python3-pip
+apt-get update && apt-get install python3 python3-pip
+#Installation du Framework et Service web qui est lié avec
+pip3 install flask Werkzeug
